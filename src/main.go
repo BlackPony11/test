34f213884b3596ab76595298fc55d5ef7af4fc16
BlackPony11 <pony89554@gmail.com>
2018-12-24 11:44:28 +0200

    Vooot

---
+++ src/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	myTimer := getTimer()
+
+	f := func() {
+		myTimer()
+	}
+
+	myTimer()
+	f()
+}
+
+func getTimer() func() {
+	start := time.Now()
+	return func() {
+		fmt.Printf("Time from start %v", time.Since(start))
+	}
+}
